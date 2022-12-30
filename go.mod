3e774c85b33bee912a66aa346165676829fb7f13
Holegots <fuergaosi@gmail.com>
2022-12-31 02:35:49 +0800

    ♿️ change code

--- go.mod
+++ go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-github/v48 v48.2.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	golang.org/x/net v0.3.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -10,14 +10,13 @@ import (
 )
 
 func main() {
-
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
 		src.LarkServer.EventCallback.ListenCallback(r.Context(), r.Body, w)
 	})
-	fmt.Println("start server ... 9726")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "9726"
 	}
-	log.Fatal(http.ListenAndServe(":9726", nil))
+	fmt.Println("start server ... ", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
